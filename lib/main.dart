4a2dc7610964b8279c8d1676141bf4db92595de6
sdoshi983 <sdoshi983@rku.ac.in>
2022-01-12 00:28:59 +0530

    firebase app initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:ecg_monitor/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
