1c6697a5ead432e68fb94ec6f7dbd4d719f596ff
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-06 14:40:02 +0530

    dashboard page started

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'ECG Monitoring',
+        ),
+      ),
+      body: Container(
+        child: ElevatedButton(
+          onPressed: (){
+            // Navi
+          },
+          child: Text(
+            'Start',
+          ),
+        ),
+      ),
+    );
+  }
+}
