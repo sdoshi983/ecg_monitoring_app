8bf1595ef68a2dc0241cdd24934964ffe11cfebd
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-06 14:52:08 +0530

    dashboard page's route

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecg_monitor/pages/dashboard.dart';
 import 'package:ecg_monitor/pages/ecg_plot.dart';
 import 'package:ecg_monitor/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: EcgPlot(),
+      home: Dashboard(),
     );
   }
 }
