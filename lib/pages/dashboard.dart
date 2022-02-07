bc6eb4f3c16c50ffdb1c717ce259fabe52dd1d44
sdoshi983 <sdoshi983@rku.ac.in>
2022-02-07 20:10:45 +0530

    dropdown added

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,8 +1,10 @@
+import 'package:ecg_monitor/helpers/constant.dart';
 import 'package:ecg_monitor/pages/ecg_plot.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+int dropDownValue = 10;
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => _DashboardState();
@@ -11,28 +13,80 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitDown,
       DeviceOrientation.portraitUp,
     ]);
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: themeColor,
         title: Text(
-            'ECG Monitoring',
+          'ECG Monitoring',
         ),
       ),
-      body: Container(
-        child: Center(
-          child: ElevatedButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => EcgPlot()));
-            },
-            child: Text(
-              'Start',
-            ),
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(primary: deepPurple),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => EcgPlot()));
+                },
+                child: Text(
+                  'Start',
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Select accuracy: ', style: textTheme.subtitle1,),
+                  AccuracyDropDown()
+                ],
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class AccuracyDropDown extends StatefulWidget {
+  @override
+  _AccuracyDropDownState createState() => _AccuracyDropDownState();
+}
+
+class _AccuracyDropDownState extends State<AccuracyDropDown> {
+  List<int> accuracies = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    return DropdownButton(
+        value: dropDownValue,
+        icon: const Icon(Icons.arrow_drop_down_outlined),
+        elevation: 16,
+        style: textTheme.subtitle1.copyWith(color: Colors.deepPurple, fontWeight: FontWeight.bold,),
+        onChanged: (newValue) {
+          setState(() {
+            dropDownValue = newValue;
+          });
+        },
+        items: accuracies.map<DropdownMenuItem<int>>(
+          (accuracy) {
+            return DropdownMenuItem<int>(
+              value: accuracy,
+              child: Text(accuracy.toString()),
+            );
+          },
+        ).toList());
   }
 }
